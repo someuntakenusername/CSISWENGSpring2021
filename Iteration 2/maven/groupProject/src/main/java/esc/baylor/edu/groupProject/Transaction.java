957fb14e0a2f04790649f183059e9c041c912197
TheLunchTrae <56175379+TheLunchTrae@users.noreply.github.com>
2021-03-29 20:15:49 -0500

    Transaction

--- Iteration 2/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/Transaction.java
+++ Iteration 2/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/Transaction.java
@@ -1,5 +1,28 @@
 package esc.baylor.edu.groupProject;
 
-public class Transaction {
+import java.util.Date;
 
+public class Transaction {
+	Double amount;
+	Date date;
+	String title;
+	
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
