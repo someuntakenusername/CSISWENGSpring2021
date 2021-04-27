0d02848dab9085006498e316e024d9515c5c5724
Will Henderson <will_henderson1@baylor.edu>
2021-04-26 21:21:32 -0500

    reporter

--- Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/TransactionObjects/Report.java
+++ Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/TransactionObjects/Report.java
@@ -2,21 +2,15 @@ package esc.baylor.edu.groupProject.TransactionObjects;
 
 import java.util.Date;
 
-public class Report implements Comparable<Report>{
+public class Report{
 	public int[] transactionIds = null;
 	public Date from = null, to = null;
-	public double savingAvg = 0,
-			savingMed = 0,
-			savingHi = 0,
-			savingLo = 0,
-			expenseAvg = 0,
-			expenseMed = 0,
-			ExpenseHi = 0,
-			ExpenseLo = 0;
-	
-	@Override
-	//sort by date
-	public int compareTo(Report o) {
-		return this.weekOf.compareTo(o.weekOf);
-	}
+	public double savingAvg = Double.NaN,
+			savingMed = Double.NaN,
+			savingHi = Double.NaN,
+			savingLo = Double.NaN,
+			expenseAvg = Double.NaN,
+			expenseMed = Double.NaN,
+			ExpenseHi = Double.NaN,
+			ExpenseLo = Double.NaN;
 }
--- Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/TransactionObjects/Reporter.java
+++ Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/TransactionObjects/Reporter.java
@@ -1,19 +1,27 @@
 package esc.baylor.edu.groupProject.TransactionObjects;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.TreeSet;
+import java.util.Vector;
+import java.util.stream.Collectors;
 
 public class Reporter {
-	private TreeSet<Report> reports;
+	private ArrayList<Report> reports;
 	
 	public Report generateReport(Date from, Date to, Collection<Transaction> t) {
 		Report report = new Report();
 		
 		report.from = from;
 		report.to = to;
+		
+		final int count = 0;
+		
+		List<Transaction> reported = t.stream().sorted((a,b) -> a.getDate().compareTo(b.getDate())).collect(Collectors.toList());
 		
 		return report;
 	}
