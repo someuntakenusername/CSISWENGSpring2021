c827f1828762a8083881b3734ce7aa7de6b11f4e
timmyFrederiksen <timmyfrederiksen@gmail.com>
2021-04-26 19:15:00 -0500

    User, write/read
    lel

--- Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/SwingGUI/User.java
+++ Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/SwingGUI/User.java
@@ -1,13 +1,12 @@
 package esc.baylor.edu.groupProject.SwingGUI;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-import esc.baylor.edu.groupProject.TransactionObjects.Category;
 import esc.baylor.edu.groupProject.TransactionObjects.TransactionLog;
 
 
@@ -43,12 +42,10 @@ public class User implements Serializable {
 	
 	public void writeUser(User serObj) {
 		try {
-		    FileOutputStream f = new FileOutputStream(new File("myObjects.txt"));
+		    FileOutputStream f = new FileOutputStream(new File("users.txt"));
 		    ObjectOutputStream o = new ObjectOutputStream(f);
 
-		    // Write objects to file
-		    o.writeObject(p1);
-		    o.writeObject(p2);
+		    o.writeObject(serObj);
 
 		    o.close();
 		    f.close();
@@ -63,50 +60,18 @@ public class User implements Serializable {
 	
 	public void load(User serObj) {
 		try {
-			ObjectInputStream objectIn = new ObjectInputStream(new FileInputStream("test.txt"));
-			objectIn.readObject();
-			objectIn.close();
-		} catch (IOException e) {
+			FileInputStream fi = new FileInputStream(new File("users.txt"));
+	    	ObjectInputStream oi = new ObjectInputStream(fi);
+	    
+	    	serObj = (User) oi.readObject();
+
+	    	oi.close();
+	    	fi.close();
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException ex) {
-			ex.printStackTrace();
 		}
-    }
-	
-	
+	}
 }
 
-try {
-    FileOutputStream f = new FileOutputStream(new File("myObjects.txt"));
-    ObjectOutputStream o = new ObjectOutputStream(f);
-
-    // Write objects to file
-    o.writeObject(p1);
-    o.writeObject(p2);
-
-    o.close();
-    f.close();
-
-    FileInputStream fi = new FileInputStream(new File("myObjects.txt"));
-    ObjectInputStream oi = new ObjectInputStream(fi);
-
-    // Read objects
-    Person pr1 = (Person) oi.readObject();
-    Person pr2 = (Person) oi.readObject();
-
-    System.out.println(pr1.toString());
-    System.out.println(pr2.toString());
-
-    oi.close();
-    fi.close();
-
-} catch (FileNotFoundException e) {
-    System.out.println("File not found");
-} catch (IOException e) {
-    System.out.println("Error initializing stream");
-} catch (ClassNotFoundException e) {
-    // TODO Auto-generated catch block
-    e.printStackTrace();
-}
 
 
