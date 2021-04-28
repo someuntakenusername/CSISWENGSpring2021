36fe9c4d108dc73371a5d94a838d2fa13cec6d96
timmyFrederiksen <timmyfrederiksen@gmail.com>
2021-04-28 17:53:17 -0500

    bleh

--- Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/System/Main.java
+++ Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/System/Main.java
@@ -1,11 +1,15 @@
 package esc.baylor.edu.groupProject.System;
 
+import java.util.logging.Logger;
+
 import esc.baylor.edu.groupProject.SwingGUI.Login;
 
 public class Main {
+	final static Logger LOGGER = Logger.getLogger("logger");
+	
 	public static void main(String[] args) {		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {            	
+            public void run() {        
             	Login g = new Login();
         		g.login();
             }
