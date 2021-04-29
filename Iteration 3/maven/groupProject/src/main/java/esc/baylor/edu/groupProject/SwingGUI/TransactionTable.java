64c181e8c0107cb36ad7146431bd49b9ae62e9ae
Will Henderson <will_henderson1@baylor.edu>
2021-04-28 20:09:01 -0500

    logging

--- Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/SwingGUI/TransactionTable.java
+++ Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/SwingGUI/TransactionTable.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
+import java.util.logging.Logger;
 
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
@@ -18,12 +19,14 @@ import esc.baylor.edu.groupProject.Objects.Category;
 import esc.baylor.edu.groupProject.Objects.TransactionLog;
 
 public class TransactionTable extends JPanel implements ActionListener {
+	private static final Logger log = Logger.getLogger(TransactionTable.class.getName());
 	private JTable table;
 	protected static TransactionTableModel model;
 	private JComboBox<Object> filter;
 
 	public TransactionTable(JComboBox<Object> filter) {
 		super(new BorderLayout());
+		log.entering(TransactionTable.class.getName(), "TransactionTable", filter);
 		this.filter = filter;
 		//Load Transaction List and Populate List Model
 		model = new TransactionTableModel();
@@ -36,33 +39,41 @@ public class TransactionTable extends JPanel implements ActionListener {
 		populate();
 		filter.setSelectedIndex(1);
 		
-		add(scroll);	
+		add(scroll);
+		log.exiting(TransactionTable.class.getName(), "TransactionTable");
 	}
 	
 	private void populate() {
+		log.entering(TransactionTable.class.getName(), "populate");
 		filter.removeAllItems(); 
 		filter.addItem("Refresh List");
 		filter.addItem("None");
 		for(int i = 0; i < model.getTransactionLog().categoryCount(); ++i) {
 			filter.addItem(model.getTransactionLog().getCategory(i));
 		}
+		log.exiting(TransactionTable.class.getName(), "populate");
 	}
 	
 	public static TransactionLog getTransactionLog() {
+		log.entering(TransactionTable.class.getName(), "getTransactionLog");
+		log.exiting(TransactionTable.class.getName(), "getTransactionLog", model.getTransactionLog());
 		return model.getTransactionLog();
 	}
 	
 	static class DecimalFormatRenderer extends DefaultTableCellRenderer {
+		private static final Logger log = Logger.getLogger(DecimalFormatRenderer.class.getName());
 		private static final DecimalFormat formatter = new DecimalFormat( "#.00" );
 	 
 	      public Component getTableCellRendererComponent(
 	         JTable table, Object value, boolean isSelected,
 	         boolean hasFocus, int row, int column) {
+	    	  log.entering(DecimalFormatRenderer.class.getName(), "getTableCellRendererComponent");
 	 
 	         // First format the cell value as required
 	         value = formatter.format((Number)value);
 	 
 	         // And pass it on to parent class
+	         log.exiting(DecimalFormatRenderer.class.getName(), "getTableCellRendererComponent", super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column ));
 	         return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column );
 	      }
 	   }
@@ -70,6 +81,7 @@ public class TransactionTable extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		log.entering(TransactionTable.class.getName(), "actionPerformed", e);
 		if(e.getActionCommand().equals("CMD_ADD_EXPENSE")) {
 			new TransactionFrame(null);
 		} else if(e.getActionCommand().equals("CMD_DELETE_EXPENSE")) {
@@ -93,5 +105,6 @@ public class TransactionTable extends JPanel implements ActionListener {
 		} else if(e.getActionCommand().equals("Set_Save")) {
 			new SaveFrame();
 		}
+		log.exiting(TransactionTable.class.getName(), "actionPerformed");
 	}
 }
