9523fbbc36d79e071c13f76642755ea178c75a56
Will Henderson <will_henderson1@baylor.edu>
2021-04-28 21:03:21 -0500

    logger

--- Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/System/Main.java
+++ Iteration 3/maven/groupProject/src/main/java/esc/baylor/edu/groupProject/System/Main.java
@@ -1,5 +1,10 @@
 package esc.baylor.edu.groupProject.System;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 import esc.baylor.edu.groupProject.SwingGUI.Login;
@@ -7,8 +12,9 @@ import esc.baylor.edu.groupProject.SwingGUI.Login;
 public class Main {
 	private static final Logger log = Logger.getLogger(Main.class.getName());
 	
-	public static void main(String[] args) {	
-		log.info("Starting BearBudget...");
+	public static void main(String[] args) throws SecurityException, IOException {	
+		Logger.getLogger("").addHandler(new FileHandler("log.txt"));
+		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {			
             public void run() {
             	Login g = new Login();
